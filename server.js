b7d67c960ceba53dc47f4efe23065e54a7e498a3
chromato99 <chromato99@gmail.com>
2022-05-27 13:49:16 +0900

    add annotation

--- server.js
+++ server.js
@@ -87,7 +87,11 @@ app.use(express.urlencoded({ extended: true }));
 //     });
 // });
 
-
+/*
+테스트용 요청
+단순히 들어온 json데이터를 다시 반환해준다.
+json 데이터가 잘 전달됬는지 확인 가능
+*/
 app.post('/test', (req, res) => {
     console.log("/test")
     console.log(req.body);
@@ -95,7 +99,61 @@ app.post('/test', (req, res) => {
 });
 
 
-// Express.js get,post request code
+/*
+데이터셋을 추가하는 요청
+json형태로 새로운 데이터를 전달받아 db에 insert해준다.
+데이터는 아래와 같은 형식으로 전달해 주면 된다.
+{
+    "position" : "307호",
+    "wifi_data" : [
+        {
+            "mac" : 111,
+            "rss" : -60
+        },
+        {
+            "mac" : 112,
+            "rss" : -30
+        },
+        {
+            "mac" : 113,
+            "rss" : -55
+        },
+        {
+            "mac" : 114,
+            "rss" : -85
+        },
+        {
+            "mac" : 116,
+            "rss" : -88
+        }
+    ]
+}
+{
+    "position" : "307호",
+    "wifi_data" : [
+        {
+            "mac" : 111,
+            "rss" : -60
+        },
+        {
+            "mac" : 112,
+            "rss" : -30
+        },
+        {
+            "mac" : 113,
+            "rss" : -55
+        },
+        {
+            "mac" : 114,
+            "rss" : -85
+        },
+        {
+            "mac" : 116,
+            "rss" : -88
+        }
+    ]
+}
+*/
 app.post('/add', (req, res) => { // Default entry
     console.log(req.body);
     let db = mysql.createConnection(db_config);
@@ -110,6 +168,37 @@ app.post('/add', (req, res) => { // Default entry
     });
 });
 
+
+/*
+위치를 찾아주는 요청
+현재 위치의 와이파이 신호 데이터를 전달하면 예상되는 위치를 반환해준다.
+위치 추정은 ./src/core.js에 정의된 함수에서 계산한다.
+데이터 형태는 add와 비슷하지만 위치를 모르는 상태이니 position값은 빼주면 된다.
+{
+    "wifi_data" : [
+        {
+            "mac" : 111,
+            "rss" : -65
+        },
+        {
+            "mac" : 112,
+            "rss" : -35
+        },
+        {
+            "mac" : 113,
+            "rss" : -60
+        },
+        {
+            "mac" : 114,
+            "rss" : -88
+        },
+        {
+            "mac" : 117,
+            "rss" : -88
+        }
+    ]
+}
+*/
 app.post('/findPosition', (req, res) => {
     console.log("/findPosition");
     console.log(req.body);
--- src/core.js
+++ src/core.js
@@ -1,37 +1,45 @@
 let mysql = require("mysql");
 const db_config = require('./db-config');
 
+
+// 위치 추정을 계산해주는 함수
 exports.findPosition = (req, res) => {
+    // 전달받은 값을 변수에 넣는다.
     let input_wifi_data = req.body.wifi_data;
     let db = mysql.createConnection(db_config);
     db.connect();
-    db.query('SELECT * FROM wifi_data', (err, db_data_arr) => {
+    db.query('SELECT * FROM wifi_data', (err, db_data_arr) => { // db에서 저장된 모든 데이터셋 호출
         if(err) {
             return res.send({ msg: "error" });
         }
         db.end();
 
+        // db에 저장된 데이터는 db_data_arr 이라는 변수에 담겨서 온다
 
+        // 아래는 추정한 위치를 담을 변수들 선언
         let best_avg = Infinity;
         let best_position = "";
         let best_count = 0;
-        // 로직을 여기에 넣으면 됨!
-        for(let db_data of db_data_arr) {
+
+
+
+        // 이 아래가 계산 로직
+        for(let db_data of db_data_arr) { // 저장된 데이터셋에서 데이터를 하나씩 불러온다.
             let sum = 0;
             let count = 0;
             let db_wifi_data = JSON.parse(db_data.wifi_data);
-            for(let one_wifi_data of db_wifi_data) {
-                let same_mac_data = input_wifi_data.filter(e => e.mac == one_wifi_data.mac);
-                if(same_mac_data.length != 0) {
+            for(let one_wifi_data of db_wifi_data) { // 데이터 셋에서 wifi 신호값을 하나씩 불러온다.
+                let same_mac_data = input_wifi_data.filter(e => e.mac == one_wifi_data.mac); // 데이터 셋에서 불러온 와이파이 ap와 같은 것이 있는지 확인
+                if(same_mac_data.length != 0) { // 데이터 셋과 같은 와이파이 신호 값이 있을때 그 신호와 얼마나 유사한지 차를 구해 계산
                     console.log("same mac");
                     console.log(same_mac_data);
                     count++;
                     sum += Math.abs(one_wifi_data.rss - same_mac_data[0].rss);
                 }
             }
-            let avg = sum / count;
+            let avg = sum / count; // 각 차의 합을 ap갯수로 나눠 평균값을 구한다.
             
-            if(count >= best_count) {
+            if(count >= best_count) { // 더 나은 값을 찾았을때 그것을 best값으로 저장
                 if(avg < best_avg) {
                     best_avg = avg;
                     best_position = db_data.position;
@@ -41,10 +49,10 @@ exports.findPosition = (req, res) => {
         }
 
 
-        if(best_avg == Infinity) {
+        if(best_avg == Infinity) { // 데이터 값을 찾지 못했을 경우 -1을 반환
             best_avg = -1;
         }
-        return res.send({
+        return res.send({ // 예측 값을 json에 담아 반환
             msg : "success",
             position : best_position,
             avg : best_avg
